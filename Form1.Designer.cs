e1fae85bce834ceb44f764684627c63389b75c63
jesus <jesusnazi8888@gmail.com>
2022-11-20 12:54:13 +0300

    Произвел некоторые изменения в интерфейсе взаимодействия с пользователем

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,42 +29,99 @@ namespace Graph
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.B_Out = new System.Windows.Forms.Button();
             this.cmd = new System.Windows.Forms.Label();
+            this.CB_Example = new System.Windows.Forms.ComboBox();
+            this.L_ArrayOfA = new System.Windows.Forms.Label();
+            this.CB_Transform = new System.Windows.Forms.ComboBox();
+            this.B_Transform = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // B_Out
             // 
-            this.button1.Location = new System.Drawing.Point(331, 407);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(429, 161);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.B_Out.Location = new System.Drawing.Point(240, 447);
+            this.B_Out.Name = "B_Out";
+            this.B_Out.Size = new System.Drawing.Size(132, 38);
+            this.B_Out.TabIndex = 0;
+            this.B_Out.Text = "вывести";
+            this.B_Out.UseVisualStyleBackColor = true;
+            this.B_Out.Click += new System.EventHandler(this.B_Out_Click);
             // 
             // cmd
             // 
             this.cmd.AutoSize = true;
-            this.cmd.Location = new System.Drawing.Point(400, 11);
-            this.cmd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cmd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd.Location = new System.Drawing.Point(276, 238);
             this.cmd.Name = "cmd";
-            this.cmd.Size = new System.Drawing.Size(46, 17);
+            this.cmd.Size = new System.Drawing.Size(46, 18);
             this.cmd.TabIndex = 1;
             this.cmd.Text = "label1";
             // 
+            // CB_Example
+            // 
+            this.CB_Example.FormattingEnabled = true;
+            this.CB_Example.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.CB_Example.Location = new System.Drawing.Point(240, 420);
+            this.CB_Example.Name = "CB_Example";
+            this.CB_Example.Size = new System.Drawing.Size(132, 21);
+            this.CB_Example.TabIndex = 2;
+            this.CB_Example.Text = "выбрать пример";
+            this.CB_Example.SelectedIndexChanged += new System.EventHandler(this.CB_Example_SelectedIndexChanged);
+            // 
+            // L_ArrayOfA
+            // 
+            this.L_ArrayOfA.AutoSize = true;
+            this.L_ArrayOfA.Font = new System.Drawing.Font("Consolas", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.L_ArrayOfA.Location = new System.Drawing.Point(291, 9);
+            this.L_ArrayOfA.Name = "L_ArrayOfA";
+            this.L_ArrayOfA.Size = new System.Drawing.Size(63, 19);
+            this.L_ArrayOfA.TabIndex = 3;
+            this.L_ArrayOfA.Text = "label1";
+            // 
+            // CB_Transform
+            // 
+            this.CB_Transform.FormattingEnabled = true;
+            this.CB_Transform.Items.AddRange(new object[] {
+            "Поиск циклов через рекурсию",
+            "Алгоритм Дейкстры",
+            "Алгоритм Краскала",
+            "Алгоритм Прима",
+            "Полнота"});
+            this.CB_Transform.Location = new System.Drawing.Point(378, 420);
+            this.CB_Transform.Name = "CB_Transform";
+            this.CB_Transform.Size = new System.Drawing.Size(132, 21);
+            this.CB_Transform.TabIndex = 5;
+            this.CB_Transform.Text = "выбрать преобразование";
+            this.CB_Transform.SelectedIndexChanged += new System.EventHandler(this.CB_Transform_SelectedIndexChanged);
+            // 
+            // B_Transform
+            // 
+            this.B_Transform.Location = new System.Drawing.Point(378, 447);
+            this.B_Transform.Name = "B_Transform";
+            this.B_Transform.Size = new System.Drawing.Size(132, 38);
+            this.B_Transform.TabIndex = 4;
+            this.B_Transform.Text = "вывести";
+            this.B_Transform.UseVisualStyleBackColor = true;
+            this.B_Transform.Click += new System.EventHandler(this.B_Transform_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1173, 612);
+            this.ClientSize = new System.Drawing.Size(880, 497);
+            this.Controls.Add(this.CB_Transform);
+            this.Controls.Add(this.B_Transform);
+            this.Controls.Add(this.L_ArrayOfA);
+            this.Controls.Add(this.CB_Example);
             this.Controls.Add(this.cmd);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.B_Out);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,8 +129,12 @@ namespace Graph
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button B_Out;
         private System.Windows.Forms.Label cmd;
+        private System.Windows.Forms.ComboBox CB_Example;
+        private System.Windows.Forms.Label L_ArrayOfA;
+        private System.Windows.Forms.ComboBox CB_Transform;
+        private System.Windows.Forms.Button B_Transform;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,22 @@ namespace Graph
 {
     public partial class Form1 : Form
     {
+        Graph g;
+        public int Selected { get; set; }
+        public int Transform { get; set; }
         public Form1()
         {
             InitializeComponent();
+            Selected = 0;
+            Transform = 0;
+            cmd.Text = "";
+            L_ArrayOfA.Text = "";
         }
+        /// <summary>
+        /// Выводит в текстовом виде массив графа
+        /// </summary>
+        /// <param name="g">Граф</param>
+        /// <param name="zn">Массив, который нужно вывести</param>
         void Print(Graph g, string zn)
         {
             int N = g.A.Count;
@@ -25,9 +37,10 @@ namespace Graph
             {
                 for (int i = 0; i < N; i++)
                 {
+                    str += "|";
                     for (int j = 0; j < N; j++)
                     {
-                        str += g.A[i][j] + " ";
+                        str += $" {g.A[i][j]}{string.Concat(Enumerable.Range(0, (3 - g.A[i][j].ToString().Length)).Select(x => " "))}|";
                     }
                     str += "\n";
                 }
@@ -40,37 +53,108 @@ namespace Graph
                 }
             }
 
-            cmd.Text = str;
+            L_ArrayOfA.Text = str;
         }
-        void Example()
+        /// <summary>
+        /// Метод выводит в текстовом виде матрицу выбранного графа
+        /// </summary>
+        /// <param name="sel">Выбранный граф</param>
+        void Example_Out(int sel)
         {
-            var A = new List<List<int>>();
             #region примеры
-            //'случайный' граф
-            //string a = " 0 1 0 1 0  1 0 1 0 0  0 1 0 1 1  1 0 1 0 1  0 0 1 1 0 ";
-            //int N = 5;
-            //пример для дейкстры
-            //string a = " 0 1 1 0  1 0 1 1  1 1 0 1  0 1 1 0 ";
-            //int N = 4;
-            //пример для краскала
-            int N = 10;
-            string a = " 0 10 0 0 20 0 0 15 0 0  10 0 5 0 0 0 0 2 8 9  0 5 0 16 0 0 0 0 4 0  0 0 16 0 28 25 0 0 11 9  20 0 0 28 0 15 0 0 0 0  0 0 0 25 15 0 10 0 7 5  0 0 0 0 0 10 0 11 0 3  15 2 0 0 0 0 11 0 4 14  0 8 4 11 0 7 0 4 0 0  0 9 0 9 0 5 3 14 0 0 ";
+            string a = "";
+            int N = 0;
+            switch (sel)
+            {
+                case 1:
+                    //случайный граф
+                    a = " 0 1 0 1 0  1 0 1 0 0  0 1 0 1 1  1 0 1 0 1  0 0 1 1 0 ";
+                    N = 5;
+                    break;
+                case 2:
+                    //пример для дейкстры
+                    a = " 0 1 1 0  1 0 1 1  1 1 0 1  0 1 1 0 ";
+                    N = 4;
+                    break;
+                case 3:
+                    //пример для краскала
+                    N = 10;
+                    a = " 0 10 0 0 20 0 0 15 0 0  10 0 5 0 0 0 0 2 8 9  0 5 0 16 0 0 0 0 4 0  0 0 16 0 28 25 0 0 11 9  20 0 0 28 0 15 0 0 0 0  0 0 0 25 15 0 10 0 7 5  0 0 0 0 0 10 0 11 0 3  15 2 0 0 0 0 11 0 4 14  0 8 4 11 0 7 0 4 0 0  0 9 0 9 0 5 3 14 0 0 ";
+                    break;
+                case 4:
+                    //'случайный' граф
+                    a = " 0 1 0 1 0  1 0 1 0 0  0 1 0 1 1  1 0 1 0 1  0 0 1 1 0 ";
+                    N = 5;
+                    break;
+                default:
+                    //'случайный' граф
+                    a = " 0 1 0 1 0  1 0 1 0 0  0 1 0 1 1  1 0 1 0 1  0 0 1 1 0 ";
+                    N = 5;
+                    break;
+            }
             #endregion
-            //----------
-            Graph g = new Graph(N, a);
+            g = new Graph(N, a);
             Print(g, "A");
-            //cmd.Text += g.a_dijkstra();   //проверен на многих/выдает залупу-пупу - логичную, но не регулируемую (как и с циклами)
+
+            //-Некоторые проверенные алгоритмы-
+            //cmd.Text += g.a_dijkstra();   //проверен на многих/выдает логичные данные, но не регулируемые (как и с циклами)
             //cmd.Text += g.a_kraskala();   //проверен на многих примерах
             //cmd.Text += g.f_multi();      //проверен на одном примере
             //cmd.Text += g.f_connect();    //на данный момент не проверен - причина описана в комменте в самом классе
             //cmd.Text += g.f_psevdo();     //проверен на одном примере
             //cmd.Text += g.f_regular();    //проверен на одном примере
-            //----------
+            //---------------------------------
         }
-        private void button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Метод осуществляет преобразование графа соответственно параметру trans - порядок установлен в соответствии с порядком файлов алгоритмов
+        /// </summary>
+        /// <param name="trans">параметр позволяющий указать преобразование</param>
+        void Transform_Out(int trans)
         {
-            Example();
+            cmd.Text = "";
+            //выборка преобразования графа соответствует порядку файлов алгоритмов
+            switch (trans)
+            {
+                case 1:
+                    //пока что не работает
+                    //cmd.Text += g.a_cycles();
+                    break;
+                case 2:
+                    cmd.Text += g.a_dijkstra();     //проверен на многих/выдает логичные данные, но не регулируемые (как и с циклами)
+                    break;
+                case 3:
+                    cmd.Text += g.a_kraskala();     //проверен на многих примерах
+                    break;
+                case 4:
+                    cmd.Text += g.a_prima();        //
+                    break;
+                case 5:
+                    cmd.Text += g.f_complete();     //
+                    break;
+                case 6:
+                    cmd.Text += g.f_connect();      //
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void B_Out_Click(object sender, EventArgs e)
+        {
+            Example_Out(Selected);
+            Transform_Out(Transform);
+        }
+        private void B_Transform_Click(object sender, EventArgs e)
+        {
+            Transform_Out(Transform);
         }
 
+        private void CB_Example_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Selected = CB_Example.SelectedIndex + 1;
+        }
+        private void CB_Transform_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Transform = CB_Transform.SelectedIndex + 1;
+        }
     }
 }
